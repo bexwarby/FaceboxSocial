44a32e83f23d03479d2bb65abb7ada99e6da9efc
JB Doffémont <jeanbernard_doffemont@yahoo.fr>
2021-10-14 11:31:03 +0200

    commit prière

---
+++ src/components/GetPublication.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <div class="publicationPost">
+        <h2 class="titlePost">{{titlePost}}</h2>
+        <p class="contentPost">{{contentPost}}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+       
+        titlePost: String,
+        contentPost: String,
+        
+        
+
+
+
+
+    }
+
+
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,41 @@
   <div class="home">
     <Navbar></Navbar>
     <PublicationContent></PublicationContent>
+    <GetPublication></GetPublication>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
 import PublicationContent from "../components/PublicationContent.vue";
+import GetPublication from "../components/GetPublication.vue";
 
 export default {
   components: {
     Navbar: Navbar,
     PublicationContent: PublicationContent,
+    GetPublication: GetPublication,
   },
+
+
+
+  methods: {
+
+    async mounted() {
+
+
+      const GetPost = "https://dw-s3-nice-facebox.osc-fr1.scalingo.io/posts";
+      const optionGetPost = {
+        method: "GET",
+        headers: {
+          "Content-Type" : "application-JSON",
+        },
+        body: {
+          page: Number,
+          limit: Number,
+        }
+      }
+    }
+  }
 };
 </script>
