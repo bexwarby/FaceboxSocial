093d2384467815dab037857d9fff89da89316612
Ludo <lc06000@gmail.com>
2021-10-14 20:17:41 +0200

    commit inscription

--- src/views/Inscription.vue
+++ src/views/Inscription.vue
@@ -1,27 +1,37 @@
 <template>
-<div>
+<div class="container">
   <div class="registration_container">
-    <div>
-      <h1>Registration</h1>
-    </div>
-    <div class="registration">
-      <label for="age">Age</label>
-      <input type="text" id="age" v-model="inputAge" />
-      <label for="occupation">Occupation</label>
-      <input type="text" id="occupation" v-model="inputOccupation" />
-      <label for="lastName">First Name : </label>
-      <input type="text" id="lastName" v-model="inputLastName" />
-      <label for="firstName">Last Name : </label>
-      <input type="text" id="firstName" v-model="inputFirstName" />
-      <label for="email">E-mail : </label>
-      <input type="text" id="email" v-model="inputEmail" />
-      <label for="password">Password : </label>
-      <input type="text" id="password" v-model="inputPassword" />
+    <div class="leftContain">
+      <img src="../assets/img/facebox.svg">
+      <p class="addvertissementFacebox"> Si tu te sens FAT</p>
+      <p class="addvertissementFacebox"> et que tu veux de venir FIT</p>
+      <p class="addvertissementFacebox"> Rejoins notre communauté de CROSSFIT</p>
 
-      <button type="submit" id="button_inscription" @click="CreateAccount">
-        Inscription
-      </button>
     </div>
+    <div class="rightContain">
+    <div>
+        <h1>Registration</h1>
+      </div>
+      <div class="registration">
+        <label for="age">Age</label>
+        <input type="text" id="age" v-model="inputAge" />
+        <label for="occupation">Occupation</label>
+        <input type="text" id="occupation" v-model="inputOccupation" />
+        <label for="lastName">First Name : </label>
+        <input type="text" id="lastName" v-model="inputLastName" />
+        <label for="firstName">Last Name : </label>
+        <input type="text" id="firstName" v-model="inputFirstName" />
+        <label for="email">E-mail : </label>
+        <input type="text" id="email" v-model="inputEmail" />
+        <label for="password">Password : </label>
+        <input type="text" id="password" v-model="inputPassword" />
+
+        <button type="submit" id="button_inscription" @click="CreateAccount">
+          Inscription
+        </button>
+      </div>
+    </div>
+  
   </div>
   <div>
     <router-link to="/">Home</router-link> |
@@ -75,15 +85,38 @@ export default {
 </script>
 
 <style>
+.container{
+  display: flex;
+  flex-direction: column;
+}
 .registration {
   display: flex;
   flex-direction: column;
 }
 
 .registration_container {
-  width: 400px;
-  height: 462px;
+  display: flex;
+  width: 1000px;
+  height: 700px;
   margin: auto;
+}
+.leftContain{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 600px;
+  height: 700px;
+  padding-right: 120px;
+  font-family: 'Lato', sans-serif;
+  font-size: 27px;
+  font-weight: 700;
+  color: #403c39;
+
+}
+
+.rightContain{
+  width: 400px;
+  height: 700px;
 }
 
 .registration_container input {
