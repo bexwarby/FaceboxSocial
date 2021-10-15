1bea219a442cccc24db116090b822a7739af4a02
Ludo <lc06000@gmail.com>
2021-10-15 19:51:02 +0200

    commit ludo modif page navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <header>
-      <img src="" />
-      <input type="text" placeholder="Recherche" />
+      <div class="logoSimplif">
+        <img src="../assets/img/f.svg" style= width:50px /> 
+      </div>
+      <div class="rechercheNavBar">
+        <input type="text" placeholder="Recherche" />
+      </div>
     </header>
 
     <div class="nav_vert_container">
       <nav class="vertical_nav">
         <div class="btn_connect">
-          <router-link to="/profil"><img src="" />Profil</router-link>
-          <router-link to="/"><img src="" />Home</router-link>
-          <router-link to=""><img src="" />Shop</router-link>
-          <router-link to=""><img src="" />Group</router-link>
+          <router-link to="/profil"><img src="../assets/img/user.svg" style= width:50px /><br>Profil</router-link>
+          <router-link to="/"><img src="../assets/img/home.svg" style= width:44px /><br>Home</router-link>
+          <router-link to=""><img src="../assets/img/boutique.svg" style= width:49px /><br>Shop</router-link>
+          <router-link to=""><img src="../assets/img/communauté.svg" style= width:49px /><br>Group</router-link>
         </div>
         <div class="btn_disconnect">
-          <router-link to="/connexion"><img src="" />Deconnexion</router-link>
+          <router-link to="/connexion"><img src=../assets/img/deconnection.svg style= width:49px /><br>Deconnexion</router-link>
         </div>
       </nav>
     </div>
@@ -25,6 +29,9 @@ export default {};
 </script>
 
 <style scoped>
+body{
+  font-family: "Lato", sans-serif;
+}
 .nav_vert_container {
   position: fixed;
   top: 50px;
@@ -32,9 +39,9 @@ export default {};
   bottom: 0;
 }
 .vertical_nav {
-  width: 100%;
+  width: 110%;
   height: 100%;
-  background-color: grey;
+  background-color: #808080;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -46,21 +53,64 @@ header {
   top: 0;
   left: 0;
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logoSimplif{
+  position: absolute;
+  left: 96px;
+}
+
+.rechercheNavBar input {
+  width: 270px;
+  color: #403c39;
+  font-size: 15px;
+}
+
+.rechercheNavBar input:focus {
+  outline: none;
+  border-width: 3px;
+  border-color: #e0a102;
+  background-color: #f1f0f1;
+  transition: 0.5s;
+
 }
 
 .btn_disconnect {
   align-items: flex-end;
+  margin-bottom: 10px;
 }
 
-.test {
-  width: 100px;
-  height: 2000px;
-  background: chocolate;
-  margin-left: 400px;
+.btn_disconnect a{
+  align-items: flex-end;
+  text-decoration: none;
+  color: #f1f0f1;
+  text-shadow: 2px 2px 2px #403c39;
 }
+
 
 .btn_connect {
   display: flex;
   flex-direction: column;
 }
+
+.btn_connect a{
+  margin-bottom: 28px;
+  padding-top: 5px;
+  text-decoration: none;
+  color: #f1f0f1;
+  text-shadow: 2px 2px 2px #403c39;
+}
+
+.btn_connect a:hover{
+  color: #e0a102;
+  transition: 0.8s;
+}
+
+
+
+
+
 </style>
